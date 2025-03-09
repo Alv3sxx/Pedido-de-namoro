b57bed76804021e0e6898bd05f399b4be68ed55e
Alv3sxx <84357486+Alv3sxx@users.noreply.github.com>
2025-03-09 17:00:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yes-button").addEventListener("click", function() {
+    document.getElementById("response-message").innerText = "Letícia, você me fez a pessoa mais feliz do mundo! ❤";
+});
+
+document.getElementById("thinking-button").addEventListener("click", function() {
+    document.getElementById("response-message").innerText = "Sem problemas! Eu espero pacientemente por sua resposta. 😊";
+});
